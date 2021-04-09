81e364a734bf9009e3ce4c212c63027a46e6bacd
Luis Felipe <felipedelta0@gmail.com>
2021-04-08 23:04:24 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,9 +1,11 @@
 package chess;
 
+import boardgame.BoardException;
+
 /**
  * @author luisf on 08/04/2021
  */
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
 
     public ChessException(String message) {
         super(message);
