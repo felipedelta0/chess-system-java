74698a8cec9447a94086f6d00a4f9034e8f069a2
Luis Felipe <felipedelta0@gmail.com>
2021-04-08 22:26:25 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,11 @@
+package chess;
+
+/**
+ * @author luisf on 08/04/2021
+ */
+public class ChessException extends RuntimeException {
+
+    public ChessException(String message) {
+        super(message);
+    }
+}
