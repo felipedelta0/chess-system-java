885daa7ccdf8602e02e67c2d1929be82fc6a028d
Luis Felipe <felipedelta0@gmail.com>
2021-04-11 18:23:15 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -25,4 +25,8 @@ public abstract class ChessPiece extends Piece {
 
         return p != null && p.getColor() != color;
     }
+
+    public ChessPosition getChessPosition() {
+        return ChessPosition.fromPosition(this.position);
+    }
 }
